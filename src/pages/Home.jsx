a9ea42cb33e35076a2dfc7f46f55f022d32c29de
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-22 00:21:16 +0200

    starting out

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="">HELLLOOO</h1>
+    </div>
+  )
+}
---
+++ src/pages/country/CountryData.jsx
@@ -0,0 +1,9 @@
+export default function CountryData({ match }) {
+  console.log(match)
+  const country = match.params.id
+  console.log(country)
+
+  return (
+    <h1></h1>
+  )
+}
