6fda8fb6d67e2268d3327439429081769816689a
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-22 18:11:06 +0200

    added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    textColor: theme => theme('colors'),
+    textColor: {
+      'white': '#FFFFFF',
+      'darkest': '#111517',
+    },
     colors: {
+      'white': '#FFFFFF',
+
       gray: {
+        whitish: '#F2F2F2',
         light: '#2B3844',
         dark: '#202C36'
       }
     },
+    extend: {},
   },
   variants: {
     extend: {},
