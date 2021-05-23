f52f66490759278877d3f4fbee3955d707c86a77
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-23 23:35:10 +0200

    added padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { TopBar } from "../components/home/_TopBar";
 
 export default function Home() {
   return (
-    <main className="py-7 px-4">
+    <main className="py-7 md:py-9 lg:py-12 px-4 md:px-8 lg:px-20">
       <TopBar />
     </main>
   )
