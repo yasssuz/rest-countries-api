90c54856d9812093c421413f406df9898d03c3d4
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-24 01:50:19 +0200

    added theme provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from "./contexts/ThemeContext";
 import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
