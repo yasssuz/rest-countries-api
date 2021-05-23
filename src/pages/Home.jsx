53549ba54fed9a84a0557c0430af6dee90cf577c
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-24 01:53:18 +0200

    added countriesList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { TopBar } from "../components/home/_TopBar";
+import { CountriesList } from "../components/home/_CountriesList";
 
 export default function Home() {
   return (
     <main className="py-7 md:py-9 lg:py-12 px-4 md:px-8 lg:px-20">
       <TopBar />
+      <CountriesList />
     </main>
   )
 }
