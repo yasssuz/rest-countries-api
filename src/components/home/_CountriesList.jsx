8faa85a6b69bfa435d87420c88b9ac45cbf53cf5
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-24 15:11:52 +0200

    mobile ready

--- src/components/home/_CountriesList.jsx
+++ src/components/home/_CountriesList.jsx
@@ -1,11 +1,12 @@
-// import { useEffect } from "react"
 import { useQuery } from 'react-query'
+import styled from 'styled-components'
+import { CountriesEskeleton } from './_CountriesEskeleton'
 
 export function CountriesList() {
-  const { isLoading, isError, data, isFetching } = getCountries()
+  const { isLoading, isError, data } = getCountries()
 
   if (isLoading) {
-    return <span>IS LOADING...</span>
+    return <CountriesEskeleton />
   }
 
   if (isError) {
@@ -16,10 +17,35 @@ export function CountriesList() {
 
   return (
     <section>
-      <ul></ul>
+      <ul className="grid gap-y-10 mt-8">
+        {data.map(country => (
+          <Item key={country.name} className="rounded-md w-full overflow-hidden bg-white dark:bg-gray-light mx-auto">
+            <img src={country.flag} alt={country.name} />
+            <div className="p-6 dark:text-white text-darkest">
+              <h2 className="dark:text-white text-lg font-extrabold mb-2">{country.name}</h2>
+              <span className="block mb-1 text-base">
+                <strong className="font-semibold">Population: </strong>
+                {country.population}
+              </span>
+              <span className="block mb-1 text-base">
+                <strong className="font-semibold">Region: </strong>
+                {country.region}
+              </span>
+              <span className="block mb-1 text-base">
+                <strong className="font-semibold">Capital: </strong>
+                {country.capital}
+              </span>
+            </div>
+          </Item>
+        ))}
+      </ul>
     </section>
   )
 }
+
+const Item = styled.li`
+  max-width: 290px;
+`
 
 function getCountries() {
   return useQuery('countries', async () => {
