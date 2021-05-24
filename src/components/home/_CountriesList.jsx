41bed5c01330ec619f40754cebcf2ee10f99c754
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-24 19:28:01 +0200

    style changes

--- src/components/home/_CountriesList.jsx
+++ src/components/home/_CountriesList.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import { GetCountries } from '../../services/getCountries'
 
 import { CountriesEskeleton } from './_CountriesEskeleton'
@@ -14,29 +15,32 @@ export function CountriesList() {
     return <span>Error</span>
   }
 
-  console.log(data)
-
   return (
     <section>
       <ul className="grid gap-y-10 mt-8">
         {data.map(country => (
-          <Item key={country.name} className="rounded-md w-full overflow-hidden bg-white dark:bg-gray-light mx-auto">
-            <img src={country.flag} alt={country.name} />
-            <div className="p-6 dark:text-white text-darkest">
-              <h2 className="dark:text-white text-lg font-extrabold mb-2">{country.name}</h2>
-              <span className="block mb-1 text-base">
-                <strong className="font-semibold">Population: </strong>
-                {country.population}
-              </span>
-              <span className="block mb-1 text-base">
-                <strong className="font-semibold">Region: </strong>
-                {country.region}
-              </span>
-              <span className="block mb-1 text-base">
-                <strong className="font-semibold">Capital: </strong>
-                {country.capital}
-              </span>
-            </div>
+          <Item
+            key={country.name}
+            className="rounded-md w-full overflow-hidden bg-white dark:bg-gray-light mx-auto"
+          >
+            <Link to={`/country/${country.name}`}>
+              <img src={country.flag} alt={country.name} />
+              <div className="p-6 dark:text-white text-darkest">
+                <h2 className="dark:text-white text-lg font-extrabold mb-2">{country.name}</h2>
+                <span className="block mb-1 text-base">
+                  <strong className="font-semibold">Population: </strong>
+                  {country.population}
+                </span>
+                <span className="block mb-1 text-base">
+                  <strong className="font-semibold">Region: </strong>
+                  {country.region}
+                </span>
+                <span className="block mb-1 text-base">
+                  <strong className="font-semibold">Capital: </strong>
+                  {country.capital}
+                </span>
+              </div>
+            </Link>
           </Item>
         ))}
       </ul>
@@ -45,5 +49,11 @@ export function CountriesList() {
 }
 
 const Item = styled.li`
-  max-width: 290px;
+  max-width: 300px;
+  transition: transform 0.2s ease, border 0.2s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    border: #ffce00 1px dashed;
+  }
 `
