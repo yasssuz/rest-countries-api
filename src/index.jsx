01e57600e0caf2b3398eb0f9f0efc09df289d0cb
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-25 01:09:07 +0200

    imports updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "./contexts/ThemeContext";
-import './styles/index.css';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
