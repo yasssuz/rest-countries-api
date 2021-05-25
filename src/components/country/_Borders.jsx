b8813ce0ee91f90933ed9a2a9e642fdad28a76c8
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-25 15:01:37 +0200

    mobile only

---
+++ src/components/country/_Borders.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+export function Borders(props) {
+  const { borders } = props
+  const [haveBorders, setHaveBorders] = useState(false)
+  let bordersArr = []
+
+  borders.map(border => {
+    bordersArr.push(border.padEnd(5, ' '))
+  })
+
+
+  useEffect(() => bordersArr[0] !== undefined && setHaveBorders(true), [])
+
+  return (
+    <div className="mt-8">
+      <h2 className="mb-4 text-darkest dark:text-white text-lg font-semibold">Border Countries:</h2>
+      <div className="grid grid-cols-3 gap-3">
+        {haveBorders ? (
+          <>
+            {bordersArr.map(country => (
+              <span key={country}
+                style={{ boxShadow: '0px 0px 7px rgba(0, 0, 0, 0.293139)' }}
+                className="text-sm dark:text-white text-darkest flex items-center justify-center w-full dark:bg-gray-light h-8 rounded-sm"
+              >
+                {country}
+              </span>
+            ))}
+          </>
+        ) : (
+          'hello'
+        )}
+      </div>
+    </div>
+  )
+}
