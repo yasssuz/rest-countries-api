2bd35a2d7bda713d740cd989bbf7d67007eb1f8e
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-25 23:24:47 +0200

    full working

---
+++ src/components/country/_LinkBtn.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export function LinkBtn(props) {
+  const { link, children } = props
+
+  return (
+    <Link
+      to={link}
+      style={{ boxShadow: '0px 0px 7px rgba(0, 0, 0, 0.293139)' }}
+      className="text-sm dark:text-white text-darkest flex items-center justify-center dark:bg-gray-light h-8 rounded-sm"
+    >
+      {children}
+    </Link >
+  )
+}
