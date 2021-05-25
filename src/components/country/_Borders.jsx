f412d102f3076de3e4de0b701c7506d22592a91c
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-25 23:26:52 +0200

    mobile only

--- src/components/country/_Borders.jsx
+++ src/components/country/_Borders.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { LinkBtn } from "./_LinkBtn"
 
 export function Borders(props) {
   const { borders } = props
@@ -10,24 +11,21 @@ export function Borders(props) {
   )
 
   return (
-    <div className="mt-8">
-      <h2 className="mb-4 text-darkest dark:text-white text-lg font-semibold">Border Countries:</h2>
-      <div className="grid grid-cols-3 gap-3">
-        {haveBorders ? (
-          <>
-            {borders.map(country => (
-              <span key={country}
-                style={{ boxShadow: '0px 0px 7px rgba(0, 0, 0, 0.293139)' }}
-                className="text-sm dark:text-white text-darkest flex items-center justify-center w-full dark:bg-gray-light h-8 rounded-sm"
-              >
-                {country}
-              </span>
+    <>
+      {haveBorders && (
+        <div className="mt-8">
+          <h2 className="mb-4 text-darkest dark:text-white text-lg font-semibold">Border Countries:</h2>
+          <ul className="grid grid-cols-3 gap-3">
+            {borders.map(code => (
+              <li key={code}>
+                <LinkBtn link="/">
+                  {code}
+                </LinkBtn>
+              </li>
             ))}
-          </>
-        ) : (
-          'hello'
-        )}
-      </div>
-    </div>
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
