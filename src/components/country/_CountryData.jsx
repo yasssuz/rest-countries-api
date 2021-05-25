47831270fc5617da9fbe1be792a28d28344826ac
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-25 23:32:11 +0200

    code refactor

--- src/components/country/_CountryData.jsx
+++ src/components/country/_CountryData.jsx
@@ -3,12 +3,14 @@ import { Info } from "./_Info"
 export function CountryData(props) {
   const { country } = props
   let currencies = []
+  let languages = []
 
   country.currencies.map(curr => currencies.push(curr.code.padEnd(5, ' ')))
+  country.languages.map(lang => languages.push(lang.name.padEnd(20, ' ')))
 
   return (
-    <div className="mt-16">
-      <img src={country.flag} alt={country.name} className="rounded-md mb-11" />
+    <div className="mt-16 sm:text-center">
+      <img src={country.flag} alt={country.name} className="rounded-md mb-11 mx-auto" />
       <h1 className="mb-4 dark:text-white text-darkest font-extrabold text-2xl">{country.name}</h1>
 
       <Info info={'Native Name'} infoRes={country.nativeName} />
@@ -20,7 +22,7 @@ export function CountryData(props) {
       <div className="mt-8">
         <Info info={'Top Level Domain'} infoRes={country.topLevelDomain} />
         <Info info={'Currencies'} infoRes={currencies} />
-        <Info info={'Languages'} infoRes={country.capital} />
+        <Info info={'Languages'} infoRes={languages} />
       </div>
     </div>
   )
