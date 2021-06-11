af4e1611e6afa0c840cecbfa034659cce9632ac2
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-11 22:19:07 +0200

    removed slice

--- src/services/getCountries.js
+++ src/services/getCountries.js
@@ -4,8 +4,7 @@ export function GetCountries() {
   return useQuery('countries', async () => {
     const res = await fetch(`https://restcountries.eu/rest/v2/all`)
     const data = await res.json()
-    const countries = data.slice(0, 8)
 
-    return countries
+    return data
   })
 }
