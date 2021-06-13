9a85d592fce739f2b78a809e19ba3d849ceb0b11
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-13 23:15:16 +0200

    added themeProvider

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
 import App from './App';
 
